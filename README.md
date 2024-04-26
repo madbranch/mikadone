bea47fea03ac9480ce197678ac372777a04ad5f4
Adam Labranche <adam@labranche.co>
2024-04-26 15:22:40 -0400

    Establish project structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mikadone
+# Mikadone
 Mikadone is a software that helps manage a todo list using the Mikado method.
---
+++ tests/Mikadone.Tests/MainViewModelTests.cs
@@ -0,0 +1,10 @@
+namespace Mikadone;
+
+public class MainViewModelTests
+{
+  [Fact]
+  public void Test1()
+  {
+
+  }
+}
