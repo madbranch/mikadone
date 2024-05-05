e5e76bcd1e367b52481ce8e760030da59f051ef5
Adam Labranche <adam@labranche.co>
2024-05-05 15:23:44 -0400

    Add missing file

---
+++ src/Mikadone/GoalSerialization.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Mikadone;
+
+public interface IGoalSerialization
+{
+  Stream Serialize(Goal goal, Stream stream);
+  Goal? Deserialize(string jsonString);
+}
+
+public class GoalSerialization : IGoalSerialization
+{
+  public Stream Serialize(Goal goal, Stream stream)
+  {
+    using Utf8JsonWriter writer = new(stream, WriterOptions);
+    Serialize(goal, writer);
+    return stream;
+  }
+
+  public Goal? Deserialize(string jsonString)
+    => JsonNode.Parse(jsonString) is not JsonObject rootNode
+      ? null
+      : Deserialize(rootNode);
+
+  private static Goal Deserialize(JsonObject node)
+    => new Goal(GetIsReached(node), GetDescription(node), GetPrerequisites(node));
+
+  private static bool GetIsReached(JsonObject node)
+    => node["isReached"] is JsonValue isReachedValue && isReachedValue.GetValueKind() == JsonValueKind.True;
+  
+  private static string GetDescription(JsonObject node)
+    => node["description"] is JsonValue descriptionValue && descriptionValue.TryGetValue(out string? description)
+    ? description
+    : string.Empty;
+  
+  private static IEnumerable<Goal> GetPrerequisites(JsonObject node)
+    => node["prerequisites"] is JsonArray prerequisitesArray
+    ? prerequisitesArray.OfType<JsonObject>().Select(Deserialize)
+    : [];
+
+  private static void Serialize(Goal goal, Utf8JsonWriter writer)
+  {
+    writer.WriteStartObject();
+    writer.WriteBoolean("isReached", goal.IsReached);
+    writer.WriteString("description", goal.Description);
+    writer.WriteStartArray("prerequisites");
+    foreach (Goal prerequisite in goal.Prerequisites)
+    {
+      Serialize(prerequisite, writer);
+    }
+    writer.WriteEndArray();
+    writer.WriteEndObject();
+  }
+
+  private static readonly JsonWriterOptions WriterOptions = new() { Indented = true };
+  private static readonly JsonReaderOptions ReaderOptions = new() { AllowTrailingCommas = true, CommentHandling = JsonCommentHandling.Skip };
+}
