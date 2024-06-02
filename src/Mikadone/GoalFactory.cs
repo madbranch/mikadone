933291eda22707f9af3f7c61f746321ffa2f7ce1
Adam Labranche <adam@labranche.co>
2024-06-02 07:31:38 -0400

    Remove blank lines

--- src/Mikadone/GoalFactory.cs
+++ src/Mikadone/GoalFactory.cs
@@ -8,15 +8,12 @@ public class GoalFactory : IGoalFactory
   private readonly IGoalIdProvider _goalIdProvider;
   private readonly IGoalEditing _goalEditing;
 
-
   public GoalFactory(IGoalIdProvider goalIdProvider, IGoalEditing goalEditing)
   {
     _goalIdProvider = goalIdProvider;
     _goalEditing = goalEditing;
   }
 
-
   public Goal CreateGoal(bool isReached, string description, IEnumerable<Goal> prerequisites)
     => new Goal(_goalIdProvider.GetNextId(), isReached, description, prerequisites, _goalEditing);
-
 }
